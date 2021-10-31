427eab1747cc1d1bde34b170537ceb036dc78366
Kieu <kieuphan530@gmail.com>
2021-10-31 15:51:37 -0500

    commit tree e876c64a907daba829c24952bedef1e375beda3d

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Write your code here
+class Breakfast {
+    constructor (food, drink) {
+        this.food = food;
+        this.drink = drink;
+
+    }
+}
+
+let mondayBreakfast = new Breakfast("cereal", "milk")
+let wednesdayBreakfast = new Breakfast ("egg sandwich", "apple juice")
+
+console.log(mondayBreakfast);
+console.log(wednesdayBreakfast);
+
+class Lunch {
+    constructor (salad, soup, drink) {
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+
+    }
+}
+
+let sundayLunch = new Lunch ("cobb", "chicken tortilla", "coke")
+let thursdayLunch = new Lunch ("caesar", "broccoli and cheddar", "water")
+
+console.log(sundayLunch);
+console.log(thursdayLunch);
+
+class Dinner {
+    constructor (salad, soup, entree, dessert) {
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this._dessert = dessert
+
+    }
+}
+
+let tuesdayDinner = new Dinner ("caesar", "broccoli and cheddar", "club sandwich", "chocolate mousse")
+let fridayDinner = new Dinner ("korean lettuce", "miso", "bbq meats", "chocolate mousse")
+
+console.log(tuesdayDinner);
+console.log(fridayDinner);
